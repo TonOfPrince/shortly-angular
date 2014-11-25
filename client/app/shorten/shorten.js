9d6bfc7c1512a8b72be8de7b54874259d2746b88
tonofprince <brettleibo@gmail.com>
2014-11-25 11:53:42 -0800

    getting links, adding links, printing out links by order of visits

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,12 +3,15 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Shorten) {
   // Your code here
   $scope.link = {};
+  // var test ={ "code": "738dd", "url": "http://www.google.com", "visits": 0, "base_url": "http://localhost:3000", "title": "Google", "_id": "5474ce069a7c22311a8ded0f", "__v": 0 }
 
-  $scope.addLink = function(){
+  $scope.addLink = function(data){
     console.log('i got called at shorten hs')
+    $scope.link['url'] = data;
     Shorten.addLink($scope.link)
       .then(function (data){
         console.log('Linked Posted', data);
+        $scope.url = '';
       })
       .catch(function (error) {
         console.error(error);
